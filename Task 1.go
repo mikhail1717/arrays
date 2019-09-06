fc4066c9d0d3d65ad45854451b21307090a14fbd
Анастасия <anastasia@MacBook-Air-Anastasia.local>
2019-09-06 14:30:19 +0300

    Initial commit

---
+++ Task 1.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sort")
+
+
+
+
+	func main() {
+		var a = readNumber()
+		var b = readNumber()
+
+			var arr1 [] int
+			for i := 0; i < a; i++{
+								{
+						arr1 = append(arr1, rand.Intn(100) )}
+							}
+		sort.Ints(arr1)
+
+			var arr2= []int {}
+			for p := 0; p < b; p++ {
+
+			{arr2 = append(arr2, rand.Intn(100))}
+
+		}
+			sort.Ints(arr2)
+
+			fmt.Println(arr1)
+			fmt.Println(arr2)
+
+
+
+}
+func readNumber() int {
+var a int
+_, _ = fmt.Scanln(&a)
+return a
+}
