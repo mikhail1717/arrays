2d754c462495774f944b689f5ed98fa898184dc4
Анастасия <anastasia@MacBook-Air-Anastasia.local>
2019-09-10 17:38:22 +0300

    Add function bubbleSort

--- Task 1.go
+++ Task 1.go
@@ -15,49 +15,15 @@ func main() {
 		arr1 = append(arr1, rand.Int()%100)
 	}
 
-	var m = len(arr1) - 1
-
-	for {
-
-		if m == 0 {
-			break
-		}
-
-		for i := 0; i < m; i++ {
-
-			if arr1[i] > arr1[i+1] {
-				arr1[i], arr1[i+1] = arr1[i+1], arr1[i]
-			}
-
-		}
-
-		m -= 1
-
-	}
-
-	var arr2 = []int{}
+	var arr2 = [] int{}
 	for p := 0; p < b; p++ {
 
 		arr2 = append(arr2, rand.Int()%100)
 	}
 
-	var n = len(arr2) - 1
-	for {
-		if n == 0 {
-			break
-		}
+	bubbleSort(arr1)
+	bubbleSort(arr2)
 
-		for i := 0; i < n; i++ {
-
-			if arr2[i] > arr2[i+1] {
-				arr2[i], arr2[i+1] = arr2[i+1], arr2[i]
-			}
-
-		}
-
-		n -= 1
-
-	}
 	fmt.Println(arr1)
 	fmt.Println(arr2)
 
@@ -66,4 +32,14 @@ func readNumber() int {
 	var a int
 	_, _ = fmt.Scanln(&a)
 	return a
+}
+
+func bubbleSort(array []int) {
+	for i := 0; i < len(array); i++ {
+		for j := 0; j < len(array)-1-i; j++ {
+			if array[j] > array[j+1] {
+				array[j], array[j+1] = array[j+1], array[j]
+			}
+		}
+	}
 }
