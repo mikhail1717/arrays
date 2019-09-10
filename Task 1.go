1565e6e5d4d9dd22e877bbd615dc61a7e0bba50c
Анастасия <anastasia@MacBook-Air-Anastasia.local>
2019-09-10 17:12:28 +0300

    Add bubble sort and Rand from 0 to 100

--- Task 1.go
+++ Task 1.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"sort"
 )
 
 func main() {
@@ -13,22 +12,52 @@ func main() {
 	var arr1 [] int
 	for i := 0; i < a; i++ {
 
-		arr1 = append(arr1, rand.Int())
+		arr1 = append(arr1, rand.Int()%100)
 	}
-	sort.Slice(arr1, func(i, j int) bool {
-		return arr1[i] < arr1 [j]
-	})
+
+	var m = len(arr1) - 1
+
+	for {
+
+		if m == 0 {
+			break
+		}
+
+		for i := 0; i < m; i++ {
+
+			if arr1[i] > arr1[i+1] {
+				arr1[i], arr1[i+1] = arr1[i+1], arr1[i]
+			}
+
+		}
+
+		m -= 1
+
+	}
 
 	var arr2 = []int{}
 	for p := 0; p < b; p++ {
 
-		arr2 = append(arr2, rand.Int())
+		arr2 = append(arr2, rand.Int()%100)
+	}
+
+	var n = len(arr2) - 1
+	for {
+		if n == 0 {
+			break
+		}
+
+		for i := 0; i < n; i++ {
+
+			if arr2[i] > arr2[i+1] {
+				arr2[i], arr2[i+1] = arr2[i+1], arr2[i]
+			}
+
+		}
+
+		n -= 1
 
 	}
-	sort.Slice(arr2, func(i, j int) bool {
-		return arr2[i] < arr2 [j]
-	})
-
 	fmt.Println(arr1)
 	fmt.Println(arr2)
 
