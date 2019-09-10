4a68dc7ccf739686b0f78caae07c1b383217e16a
Анастасия <anastasia@MacBook-Air-Anastasia.local>
2019-09-10 14:07:22 +0300

    Make some changes

--- Task 1.go
+++ Task 1.go
@@ -3,40 +3,38 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"reflect"
 	"sort"
 )
 
- func main() {
-		var a = readNumber()
-		var b = readNumber()
+func main() {
+	var a = readNumber()
+	var b = readNumber()
 
-			var arr1 [] int
-			for i := 0; i < a; i++{
+	var arr1 [] int
+	for i := 0; i < a; i++ {
 
-						arr1 = append(arr1, rand.Int() )
-							}
-		sort.Slice(arr1, func(i, j int) bool {
-			return arr1[i] < arr1 [j]
-		})
+		arr1 = append(arr1, rand.Int())
+	}
+	sort.Slice(arr1, func(i, j int) bool {
+		return arr1[i] < arr1 [j]
+	})
 
-			var arr2= []int {}
-			for p := 0; p < b; p++ {
+	var arr2 = []int{}
+	for p := 0; p < b; p++ {
 
-			arr2 = append(arr2, rand.Int())
+		arr2 = append(arr2, rand.Int())
 
-		}
-	 sort.Slice(arr2, func(i, j int) bool {
-		 return arr2[i] < arr2 [j]
-	 })
+	}
+	sort.Slice(arr2, func(i, j int) bool {
+		return arr2[i] < arr2 [j]
+	})
 
-			fmt.Println(arr1)
-			fmt.Println(arr2)
-				fmt.Println(reflect.DeepEqual(arr1, arr2))
+	fmt.Println(arr1)
+	fmt.Println(arr2)
 
 }
 func readNumber() int {
-var a int
-_, _ = fmt.Scanln(&a)
-return a
+	var a int
+	_, _ = fmt.Scanln(&a)
+	return a
 }
