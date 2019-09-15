0bedc1b321bf3abda8ab81296c47d28f1e7ae249
Анастасия <anastasia@MacBook-Air-Anastasia.local>
2019-09-15 17:09:33 +0300

    bubbleTask1 complete

--- Task 1.go
+++ Task 1.go
@@ -20,12 +20,19 @@ func main() {
 
 		arr2 = append(arr2, rand.Int()%100)
 	}
+	fmt.Println(arr1)
+	fmt.Println(arr2)
+	fmt.Println("")
+	fmt.Println("Сортировка ")
 
 	bubbleSort(arr1)
 	bubbleSort(arr2)
 
+	fmt.Println("")
 	fmt.Println(arr1)
 	fmt.Println(arr2)
+	fmt.Println("")
+	compare(arr1, arr2)
 
 }
 func readNumber() int {
@@ -40,6 +47,24 @@ func bubbleSort(array []int) {
 			if array[j] > array[j+1] {
 				array[j], array[j+1] = array[j+1], array[j]
 			}
+		}
+	}
+}
+
+func compare(arr1, arr2 []int) {
+	for i := 0; i < len(arr1) || i < len(arr2); i++ {
+		if len(arr1) <= i {
+			fmt.Println("_")
+		} else if len(arr2) <= i {
+			fmt.Println("_")
+		} else if arr1[i] < arr2[i] {
+			fmt.Println(arr1[i], "<", arr2[i])
+		} else if arr1[i] > arr2[i] {
+			fmt.Println(arr1[i], ">", arr2[i])
+		} else if arr1[i] == arr2[i] {
+			fmt.Println(arr1[i], "=", arr2[i])
+		} else {
+			fmt.Println("_")
 		}
 	}
 }
